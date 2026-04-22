8f7b25a92b0697edd42f687bec3cdea4cb70c340
Mat <matranadorxd@gmail.com>
2026-04-22 17:12:23 -0600

    Add files via upload

--- model.js
+++ model.js
@@ -14,9 +14,9 @@
 
         setExplain(
             "La carencia de acceso a los servicios de salud ah llevado a que muchos ciudadanos no cuenten con los servicios adecuados para" +
-            "la salud, llevando a que mas de 44.5 millones de personas sean afectadas y no cuenten con los tratamientos debidos, pues no mas" +
-            "de 37% de los ciudadanos ejercen esta carrera por el temor o la gran responsabilidad que presenta disminuyendo su seguridad" +
-            "y el acceso que puede tener la gente a su salud."
+            " la salud, llevando a que mas de 44.5 millones de personas sean afectadas y no cuenten con los tratamientos debidos, pues no mas" +
+            " del 37% de los ciudadanos ejercen esta carrera por el temor o la gran responsabilidad que presenta disminuyendo la seguridad" +
+            " y el acceso que puede tener muchos ante su salud."
         );
     }
 
@@ -28,9 +28,9 @@
 
         setExplain(
             "La carencia social es uno de los factores mas comunes en la poblacion que bien, no se 'consideran' pobres como tal la carencia" +
-            "de recursos, tales como La educacion, La salud, Los servicios basicos como (Luz, agua potable, gas) y en algunos casos, de" +
-            "viviendas propias, llevando a que mucha gente tome multiples trabajos para sobrevivir el dia a dia, esto representando el 32%" +
-            "de la poblacion total (segun la 4t), teniendo un gran impacto en la seguridad y en el crecimiento economico."
+            " de recursos, tales como La educacion, La salud, Los servicios basicos como (Luz, agua potable, gas) y en algunos casos, de" +
+            " viviendas propias, llevando a que mucha gente tome multiples trabajos para sobrevivir el dia a dia, esto representando el 32%" +
+            " de la poblacion total (segun la 4t), teniendo un gran impacto en la seguridad y en el crecimiento economico."
         );
     }
 
@@ -42,9 +42,9 @@
 
         setExplain(
             "La pobreza extrema es uno de las situaciones mas lamentables para la poblacion pero que por fortuna no ah ido en crecimiento," +
-            "desde 2024 esa cifra se ah mantenido estable, representando el 0.11% de la poblacion, en su mayor caso muchos no reciben ingresos" +
-            "de forma directa, ya sea por incapacidades oh conflitos sociales, familiares o por abandono, por lo que muchos, dia a dia tratan" +
-            "de obtener sustento por pequeños apoyos sociales, asi como ofreciendo servicios que estan bajo a su disposicion."
+            " desde 2024 esa cifra se ah mantenido estable, representando el 0.11% de la poblacion, en su mayor caso muchos no reciben ingresos" +
+            " de forma directa, ya sea por incapacidades oh conflitos sociales, familiares o por abandono, por lo que muchos, dia a dia tratan" +
+            " de obtener sustento por pequeños apoyos sociales, asi como ofreciendo servicios que estan bajo a su disposicion."
         );
     }
 
@@ -56,9 +56,9 @@
 
         setExplain(
             "La escazes salarial es un tema que ah ido mejorando poco a poco en los ultimos dias, pues desde el 2025 esta taza ah caido al 32%" +
-            "de las personas que no pueden comprar la canasta basica con su salario, debido a un pequeño aumento en el salario minimo y el" +
-            "crecimiento de empleos para que la gente tenga de donde generar para dar sustento a sus hogares y a la vez obtener los servicios" +
-            "requeridos para poder vivir su dia a dia."
+            " de las personas que no pueden comprar la canasta basica con su salario, debido a un pequeño aumento en el salario minimo y el" +
+            " crecimiento de empleos para que la gente tenga de donde generar para dar sustento a sus hogares y a la vez obtener los servicios" +
+            " requeridos para poder vivir su dia a dia."
         );
     }
 
